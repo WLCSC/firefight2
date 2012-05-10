b7789cfd7e71387d73c8c81ef2d0c0f30f0e7fbe
Brad Thompson <brad@bradsotherbrain.com>
2012-05-10 11:05:06 -0400

    Fixed new/edit asset form

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -8,9 +8,30 @@ class Asset < ActiveRecord::Base
 	has_many :tickets
 	has_many :returns
 	has_many :loans, :through => :returns
+	before_save :fix_room
 
-	attr_accessible :room_id, :tag, :rtype_id, :os, :manufacturer_id, :model_id, :serial, :purchase, :cost, :vendor_id, :notes, :super, :name
+	attr_accessor :room_name, :purchase_date, :model_name
+	attr_accessible :room_id, :tag, :rtype_id, :os, :manufacturer_id, :model_id, :serial, :purchase, :cost, :vendor_id, :notes, :super, :name, :room_name
 
 	validates_presence_of :tag
 	validates_uniqueness_of :tag
+
+	def fix_room
+		if self.room_name
+			r = self.room_name.split(' - ')
+			self.room = Room.where(:name => r[1]).where(:building_id => Building.where(:name => r[0]).first.id).first
+		end
+
+		if self.purchase_date
+			self.purchase_date.match /(\d+)\/(\d+)\/(\d+)/
+			self.purchase = Date.civil($3.to_i, $1.to_i, $2.to_i)
+		end
+
+		if self.model_name
+			r = self.model_name.split(' - ')
+			self.model = Model.where(:name => r[1]).where(:manufacturer_id => Manufacturer.where(:name => r[0]).first.id).first
+		end
+
+
+	end
 end
