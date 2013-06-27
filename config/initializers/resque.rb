c490afaa8eaf4d0653bca31a2de931dad381703e
Brad Thompson <brad@bradsotherbrain.com>
2013-06-27 10:41:09 -0400

    more resque fun

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -3,3 +3,4 @@ rails_env = ENV['RAILS_ENV'] || 'development'
 
 resque_config = YAML.load_file("#{Rails.root}/config/resque.yml")
 Resque.redis = resque_config[rails_env]
+Resque::Mailer.default_queue_name = 'firefight'
