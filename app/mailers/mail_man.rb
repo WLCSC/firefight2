c62e3f9cd9b3377d8dbbe8e1336e26c6a2da6312
Kristofer Rye <kristofer.rye@gmail.com>
2013-08-02 14:33:38 -0400

    we don't HAVE to send email
    and we certainly shouldn't depend on the ability to

--- app/mailers/mail_man.rb
+++ app/mailers/mail_man.rb
@@ -1,60 +1,63 @@
 class MailMan < ActionMailer::Base
-	include Resque::Mailer
-  default :from => "noreply@wl.k12.in.us"
-  
-  def test to
-	mail(:to => to, :subject => 'Firefight Mail Test')
-  end
-  
-  def ticket_submitted user, ticket, comment
-    @user = User.find(user)
-	@ticket = Ticket.find(ticket)
-	@comment = Comment.find(comment)
-	mail(:to => @user.email, :subject => "Ticket ##{@ticket.id}")
-  end
+	if APP_CONFIG[:use_resque]
+		include Resque::Mailer
+	end
+	
+	default :from => "noreply@wl.k12.in.us"
+	
+	def test to
+		mail(:to => to, :subject => 'Firefight Mail Test')
+	end
 
-  def tech_submitted user, ticket, comment
-	@user = User.find(user)
-	@ticket = Ticket.find(ticket)
-	@comment = Comment.find(comment)
-	mail(:to => @user.email, :subject => "Ticket ##{@ticket.id}")
-  end
-  
-  def ticket_updated ticket, user
-	@ticket = Ticket.find(ticket)
-	@user = User.find(user)
-	mail(:to => @user.email, :subject => "Ticket ##{@ticket.id}")
-  end
+	def ticket_submitted user, ticket, comment
+		@user = User.find(user)
+		@ticket = Ticket.find(ticket)
+		@comment = Comment.find(comment)
+		mail(:to => @user.email, :subject => "Ticket ##{@ticket.id}")
+	end
 
-  def loan_submitted user, loan
-    @user = User.find(user)
-    @loan = Loan.find(loan)
-    mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
-  end
+	def tech_submitted user, ticket, comment
+		@user = User.find(user)
+		@ticket = Ticket.find(ticket)
+		@comment = Comment.find(comment)
+		mail(:to => @user.email, :subject => "Ticket ##{@ticket.id}")
+	end
+	
+	def ticket_updated ticket, user
+		@ticket = Ticket.find(ticket)
+		@user = User.find(user)
+		mail(:to => @user.email, :subject => "Ticket ##{@ticket.id}")
+	end
 
-  def tech_loan user, loan
-    @user = User.find(user)
-    @loan = Loan.find(loan)
-    mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
-  end
+	def loan_submitted user, loan
+		@user = User.find(user)
+		@loan = Loan.find(loan)
+		mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
+	end
 
-  def loan_approved user, loan
-    @user = User.find(user)
-    @loan = Loan.find(loan)
-    mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
-  end
+	def tech_loan user, loan
+		@user = User.find(user)
+		@loan = Loan.find(loan)
+		mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
+	end
+
+	def loan_approved user, loan
+		@user = User.find(user)
+		@loan = Loan.find(loan)
+		mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
+	end
 
 	def loan_denied user, loan
-    @user = User.find(user)
-    @loan = Loan.find(loan)
-    mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
-  end
+		@user = User.find(user)
+		@loan = Loan.find(loan)
+		mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
+	end
 
-  def loan_return user, loan
-    @user = User.find(user)
-    @loan = Loan.find(loan)
-    mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
-  end
+	def loan_return user, loan
+		@user = User.find(user)
+		@loan = Loan.find(loan)
+		mail(:to => @user.email, :subject => "Tech Equipment Loan ##{@loan.id}")
+	end
 
 	def loan_remind user, loan
 		@loan = Loan.find(loan)
@@ -68,6 +71,4 @@ class MailMan < ActionMailer::Base
 		@building = @alert.building
 		mail(:to => @user.email, :subject => "Consumable Alert #{Time.now.strftime("%I:%M %p %m-%d-%Y")}")
 	end
-
-
 end
