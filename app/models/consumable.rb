a89a6ee18bfc9f9e7f1b834c2c40b5b600f5c3e2
Brad Thompson <thompsonb@wl.k12.in.us>
2014-06-24 09:42:08 -0400

    add consumable validation

--- app/models/consumable.rb
+++ app/models/consumable.rb
@@ -6,6 +6,7 @@ class Consumable < ActiveRecord::Base
 	has_many :alerts
 	has_many :uses
 	has_many :used_in_rooms, :through => :uses, :foreign_key => :room_id
+    validates :target, presence: true
 
 	def total_count
 		self.inventories.map{|i| i.count}.inject(0,:+)
