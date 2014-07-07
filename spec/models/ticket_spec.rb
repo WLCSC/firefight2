8a37f57c9f95980330ba48885d0f6bebf2010f1d
Kristofer Rye <kristofer.rye@gmail.com>
2014-07-07 00:20:50 -0400

    Start writing tests.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,4 +1,65 @@
 require "spec_helper"
 
 describe Ticket do
+	describe :statusify do
+		it "returns a string using predefined value of 1 (low priority)" do
+			t = Ticket.new(status: 1)
+
+			expect(t.statusify).to be_a(String)
+		end
+
+		it "returns a string using predefined value of 2 (routine priority)" do
+			t = Ticket.new(status: 2)
+
+			expect(t.statusify).to be_a(String)
+		end
+
+		it "returns a string using predefined value of 3 (urgent priority)" do
+			t = Ticket.new(status: 3)
+
+			expect(t.statusify).to be_a(String)
+		end
+
+		it "returns a string using predefined value of 99 (deferred status)" do
+			t = Ticket.new(status: 99)
+
+			expect(t.statusify).to be_a(String)
+		end
+
+		it "returns a string using predefined value of 100 (completed status)" do
+			t = Ticket.new(status: 100)
+
+			expect(t.statusify).to be_a(String)
+		end
+
+		it "returns the correct string using predefined value of 1 (low priority)" do
+			t = Ticket.new(status: 1)
+
+			expect(t.statusify).to eq("Low")
+		end
+
+		it "returns the correct string using predefined value of 2 (routine priority)" do
+			t = Ticket.new(status: 2)
+
+			expect(t.statusify).to eq("Routine")
+		end
+
+		it "returns the correct string using predefined value of 3 (urgent priority)" do
+			t = Ticket.new(status: 3)
+
+			expect(t.statusify).to eq("Urgent")
+		end
+
+		it "returns the correct string using predefined value of 99 (deferred status)" do
+			t = Ticket.new(status: 99)
+
+			expect(t.statusify).to eq("Deferred")
+		end
+
+		it "returns the correct string using predefined value of 100 (completed status)" do
+			t = Ticket.new(status: 100)
+
+			expect(t.statusify).to eq("Completed")
+		end
+	end
 end
